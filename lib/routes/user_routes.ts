ef9ad63d53c505b21857a5ad393af00d03f405a5
Robertguarneros <roberto.guarneros11@gmail.com>
2024-03-12 13:46:21 +0100

    Modified user_routes

--- lib/routes/user_routes.ts
+++ lib/routes/user_routes.ts
@@ -7,20 +7,27 @@ export class UserRoutes {
 
     public route(app: Application) {
         
-        app.post('', (req: Request, res: Response) => {
-            this.user_controller.create_user(req, res);
+        app.post('/users', (req: Request, res: Response) => {
+            this.user_controller.register_user(req, res);
         });
 
-        app.get('/:id', (req: Request, res: Response) => {
+        app.get('/users/:id', (req: Request, res: Response) => {
             this.user_controller.get_user(req, res);
         });
 
-        app.put('/:id', (req: Request, res: Response) => {
+        //For the routes The page and pageSize parameters can be provided as query parameters:
+        // e.g., /users?page=2&pageSize=10, to specify the page number and the number of users per page. 
+        //If these parameters are not provided, default values will be used (page 1, pageSize 10).
+        app.get('/users', (req: Request, res: Response) => {
+            this.user_controller.get_users(req, res);
+        });
+
+        app.put('/users/:id', (req: Request, res: Response) => {
             this.user_controller.update_user(req, res);
         });
 
-        app.delete('/:id', (req: Request, res: Response) => {
-            this.user_controller.delete_user(req, res);
+        app.delete('/users/:id', (req: Request, res: Response) => {
+            this.user_controller.deactivate_user(req, res);
         });
 
     }
